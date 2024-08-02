48e9a7fc25977a0f246ef448800e2e1fbdbe7a7d
Lais Lima <laissp25@gmail.com>
2024-08-02 20:42:29 -0300

    Input

---
+++ TeoPY/Dia02/Teo02.py
@@ -0,0 +1,6 @@
+# Entrada de dados
+
+x = input("Digite x: \n")
+print (x)
+
+# Quebra de linha utiliza o \n
---
+++ TeoPY/Dia02/variaveis.py
@@ -0,0 +1,17 @@
+# %%
+nome = "Téo Calvo"
+print(nome)
+
+# %%
+print("Seja bem vindo,", nome)
+
+# %%
+nome = "Laís"
+print(nome)
+
+# %%
+a = 10
+b = 20
+resultado = a**b
+print(resultado)
+# %%
