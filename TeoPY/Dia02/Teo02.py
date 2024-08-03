30d9ad4e41c2d67ea7885523387ff0bab79479cb
Lais Lima <laissp25@gmail.com>
2024-08-03 00:04:59 -0300

    Exercícios 1 parte

--- TeoPY/Dia02/Teo02.py
+++ TeoPY/Dia02/Teo02.py
@@ -3,4 +3,15 @@
 x = input("Digite x: \n")
 print (x)
 
+# %% o input irá ler em formato de texto, para ler outros formatos e necessário converter o formato. Ex:
+
+quebrado = float(input("Digite o número: \n"))
+print (quebrado)
+
+
+
 # Quebra de linha utiliza o \n
+# %%
+
+
+# O input pode ser usado para interromper um programa ao dar enter
---
+++ TeoPY/Exercicioscurso1/historia.py
@@ -0,0 +1,17 @@
+""" Crie uma história simples. Adicione essa história em um programa. A cada
+parágrafo, a história deve aguardar o usuário apertar “enter” para dar
+continuidade. """
+
+# %%
+
+print("Digite enter quando quiser ler o próximo parágrafo")
+input()
+
+print("1111111111111111111")
+input()
+
+print("bbbbbbbbbbbbbbbbbbbb")
+input()
+
+print("ccccccccccccccccc")
+input()
