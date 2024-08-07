01287140ff03c0075a01b94a8651b2ddcdcfd636
Lais Lima <laissp25@gmail.com>
2024-08-06 23:12:01 -0300

    Bhaskara 2.0

--- CourseraUSP/bhaskara.py
+++ CourseraUSP/bhaskara.py
@@ -1,28 +1,21 @@
 import math
 
-def delta (a, b, c):
-    return b**2-4*a*c
-
+# Entrada dos coeficientes a, b, e c
 a = float(input("Digite o valor de a: "))
 b = float(input("Digite o valor de b: "))
 c = float(input("Digite o valor de c: "))
 
+# Cálculo do discriminante
+delta = b**2 - 4*a*c
 
-if delta(a, b, c) == 0: 
-    raiz1 = (-b + math.sqrt(delta(a, b, c)))/2*a
-    print(" A unica raiz é: ", raiz1)
+# Verificação das condições do delta
+if delta == 0:
+    raiz1 = (-b + math.sqrt(delta)) / (2*a)
+    print("A única raiz é:", raiz1)
+elif delta < 0:
+    print("A equação não possui raízes reais")
 else:
-    if delta() <0:
-        print("a equação não possui raizes reais")
-    else: 
-        raiz1 = (-b + math.sqrt(delta(a, b, c)))/2*a
-        raiz2 = (-b - math.sqr(delta(a, b, c)))/2*a
-        print("A primeira raiz é: ", raiz1)
-        print("A segunda raiz é: ", raiz2)
-
-
-
-
-
-
-
+    raiz1 = (-b + math.sqrt(delta)) / (2*a)
+    raiz2 = (-b - math.sqrt(delta)) / (2*a)
+    print("A primeira raiz é:", raiz1)
+    print("A segunda raiz é:", raiz2)
